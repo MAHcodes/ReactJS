2fb900a698637503a6efc40e30c5a448808ce474
mhmdali102 <mhmdali102@bk.ru>
2021-11-07 23:40:24 +0200

    movies-app first commit

---
+++ movies-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Movies app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
