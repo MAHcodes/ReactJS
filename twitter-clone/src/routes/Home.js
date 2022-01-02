96f777c7e6c4291dbdb78985fb8d27f4541c681f
mhmdali102 <mhmdali102@bk.ru>
2022-01-02 06:16:15 +0200

    real time db firebase

--- twitter-clone/src/routes/Home.js
+++ twitter-clone/src/routes/Home.js
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import styles from "./Home.module.css";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 
 const Home = () => {
   const [posts, setPosts] = useState("");
-  //useEffect(() => {
-  //(async function (db) {
-  //const postsCol = collection(db, "posts");
-  //const postSnapshot = await getDocs(postsCol);
-  //const postList = postSnapshot.docs.map((doc) => ({
-  //id: doc.id,
-  //data: doc.data(),
-  //}));
-  //setPosts(JSON.stringify(postList));
-  //})(db);
-  //}, []);
+  useEffect(() => {
+    (async function (db) {
+      const postsCol = collection(db, "posts");
+      onSnapshot(postsCol, (snapshot) => {
+        const postList = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPosts(JSON.stringify(postList));
+        console.log(postList);
+      });
+    })(db);
+  }, []);
 
   return <div className={styles.home}> Home {posts}</div>;
 };
